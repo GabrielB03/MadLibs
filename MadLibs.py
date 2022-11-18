dcbc8e70ac76e5d0ed6a4e8cdc18e90b5e409da7
GabrielB03 <bielbomfim03@gmail.com>
2022-11-17 21:58:45 -0300

    MadLibs in Python MadLibs em Python
    Project of MadLibs in Python.
    Projeto de MadLibs em Python.

---
+++ MadLibs.py
@@ -0,0 +1,23 @@
+#MadLibs program
+
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous person: ")
+
+madLib = f"Computer programming is so {adj}! It makes me so excited all the time because \
+I love to {verb1}. Stay hidrated and {verb2} like you are {famous_person}."
+
+print(madLib)
+
+#Programa de MadLibs
+
+#adj = input("Adjetivo: ")
+#verbo1 = input("Verbo: ")
+#verbo2 = input("Verbo: ")
+#pessoa_famosa = input("Pessoa famosa: ")
+
+#madLib = f"Programação de computador é tão {adj}! Isso me deixa tão animado o tempo todo porque \
+#eu amo {verbo1}. Mantenha-se hidratado e {verbo2} como se você fosse {pessoa_famosa}."
+
+#print(madLib)
